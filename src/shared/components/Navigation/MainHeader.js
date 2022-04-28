779d9a8297b74555df2b15d91a743b5e22940165
Sezer Bozkır <admin@sezerbozkir.com>
2022-04-28 10:34:13 +0300

    Hamburger menu is added

--- src/shared/components/Navigation/MainHeader.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import './MainHeader.css'
-
-const MainHeader = () => {
-
-}
-export default MainHeader;
--- src/shared/components/Navigation/MainNavigation.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import './MainNavigation.css'
-
-const MainNavigation = () => {
-
-}
-export default MainNavigation
